07e72e83aa4fd245451661adf8102e3859cf5d18
Ramon Ribeiro <ramon_20112011@hotmail.com>
2021-11-27 20:23:18 -0300

    feat: cria exemplo de evento

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Home = () => {
-  return <div></div>;
+  const handleClick = () => {};
+
+  return (
+    <div>
+      <button onClick={handleClick}>Clique aqui</button>
+    </div>
+  );
 };
 
 export { Home };
