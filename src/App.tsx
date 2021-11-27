fdea08513fcf0277d85de51880d38189ae7077b4
Ramon Ribeiro <ramon_20112011@hotmail.com>
2021-11-27 19:39:13 -0300

    feat: cria exemplo de props com tipagem

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+interface LinkProps {
+  text: string;
+}
+
+function Link(props: LinkProps) {
+  return <h2>{props.text}</h2>;
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Link text="Link 01" />
     </div>
   );
 }
